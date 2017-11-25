c21e0892d77460011e139ce4bd129d48c223d251
ptudgd <nguyen.hoang.duy1997@gmail.com>
2017-11-26 00:45:31 +0700

    Upload

---
+++ Banhang.Solution/NhaCungCapForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using NhaCungCap.Repository;
+
+namespace NhaCungCapForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using(var cmd = new NhaCungCapListRepository())
+            {
+                this.nhaCungCapBindingSource.DataSource = cmd.Execute();
+            }
+        }
+    }
+}
